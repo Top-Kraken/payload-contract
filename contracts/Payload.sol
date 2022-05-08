68944ca70c9d5dd9f6af3e02b4e93a7640cab06e
Top-Kraken <topkyle521@gmail.com>
2022-05-08 11:39:37 +0900

    add modifier parts

--- contracts/Payload.sol
+++ contracts/Payload.sol
@@ -13,12 +13,27 @@ contract Payload {
     uint256 public param5;
     uint256 public param6;
 
-    uint256 private constant defaultValueForParam1 = 3000;
-    uint256 private constant defaultValueForParam2 = 3000;
-    uint256 private constant defaultValueForParam3 = 3000;
+    uint256 private constant defaultValueForParam1 = 400000000000000000;
+    uint256 private constant defaultValueForParam2 = 400000000000000000;
+    uint256 private constant defaultValueForParam3 = 1400000000000000000;
     address private constant defaultValueForParam4 = 0xF02Db5737949a0fe8Ff070399e419E3bad334A12;
-    uint256 private constant defaultValueForParam5 = 3000;
-    uint256 private constant defaultValueForParam6 = 3000;
+    uint256 private constant defaultValueForParam5 = 400000000000000000;
+    uint256 private constant defaultValueForParam6 = 1400000000000000000;
+
+    modifier betweenZeroAndOne(uint256 param) {
+        require(param > 0 && param < 1000000000000000000, "Param must be greater than 0 and less than 1.");
+        _;
+    }
+
+    modifier betweenOneAndTwo(uint256 param) {
+        require(param > 1000000000000000000 && param < 2000000000000000000, "Param must be greater than 1 and less than 2.");
+        _;
+    }
+
+    modifier notZeroAddress(address param) {
+        require(param != address(0), "Param cannot be zero address.");
+        _;
+    }
 
     constructor() {
         
@@ -36,6 +51,7 @@ contract Payload {
     }
 
     function M(uint _param1)
+        betweenZeroAndOne(_param1)
         public
     {
         param1 = _param1;
@@ -47,6 +63,8 @@ contract Payload {
     }
 
     function M(uint _param1, uint _param2)
+        betweenZeroAndOne(_param1)
+        betweenZeroAndOne(_param2)
         public
     {
         param1 = _param1;
@@ -58,6 +76,9 @@ contract Payload {
     }
 
     function M(uint _param1, uint _param2, uint _param3)
+        betweenZeroAndOne(_param1)
+        betweenZeroAndOne(_param2)
+        betweenOneAndTwo(_param3)
         public
     {
         param1 = _param1;
@@ -69,6 +90,10 @@ contract Payload {
     }
 
     function M(uint _param1, uint _param2, uint _param3, address _param4)
+        betweenZeroAndOne(_param1)
+        betweenZeroAndOne(_param2)
+        betweenOneAndTwo(_param3)
+        notZeroAddress(_param4)
         public
     {
         param1 = _param1;
@@ -80,6 +105,11 @@ contract Payload {
     }
 
     function M(uint _param1, uint _param2, uint _param3, address _param4, uint _param5)
+        betweenZeroAndOne(_param1)
+        betweenZeroAndOne(_param2)
+        betweenOneAndTwo(_param3)
+        notZeroAddress(_param4)
+        betweenZeroAndOne(_param5)
         public
     {
         param1 = _param1;
@@ -91,6 +121,12 @@ contract Payload {
     }
 
     function M(uint _param1, uint _param2, uint _param3, address _param4, uint _param5, uint _param6)
+        betweenZeroAndOne(_param1)
+        betweenZeroAndOne(_param2)
+        betweenOneAndTwo(_param3)
+        notZeroAddress(_param4)
+        betweenZeroAndOne(_param5)
+        betweenOneAndTwo(_param6)
         public
     {
         param1 = _param1;
--- test/system.test.js
+++ test/system.test.js
@@ -1,19 +1,27 @@
 const { expect, assert } = require("chai");
 const { ethers } = require("hardhat");
+const { BigNumber } = require("bignumber.js");
 
-const param1 = 3000;
-const param2 = 3000;
-const param3 = 3000;
+const param1 = ethers.utils.parseUnits("4", 17);
+const param2 = ethers.utils.parseUnits("4", 17);
+const param3 = ethers.utils.parseUnits("14", 17);
 const param4 = '0xF02Db5737949a0fe8Ff070399e419E3bad334A12';
-const param5 = 3000;
-const param6 = 3000;
+const param5 = ethers.utils.parseUnits("4", 17);
+const param6 = ethers.utils.parseUnits("14", 17);
 
-const updatedParam1 = 1000;
-const updatedParam2 = 1001;
-const updatedParam3 = 1002;
+const updatedParam1 = ethers.utils.parseUnits("1", 17);
+const updatedParam2 = ethers.utils.parseUnits("2", 17);
+const updatedParam3 = ethers.utils.parseUnits("11", 17);
 const updatedParam4 = '0xb6B466eeFd42665619a00001f28fC0AC2294B41a';
-const updatedParam5 = 1003;
-const updatedParam6 = 1004;
+const updatedParam5 = ethers.utils.parseUnits("3", 17);
+const updatedParam6 = ethers.utils.parseUnits("12", 17);
+
+const revertParam1 = ethers.utils.parseUnits("11", 17);
+const revertParam2 = ethers.utils.parseUnits("11", 17);
+const revertParam3 = ethers.utils.parseUnits("4", 17);
+const revertParam4 = '0x0000000000000000000000000000000000000000';
+const revertParam5 = ethers.utils.parseUnits("11", 17);
+const revertParam6 = ethers.utils.parseUnits("4", 17);
 
 const msgIncorrectParam1 = "Incorrect value for Param1";
 const msgIncorrectParam2 = "Incorrect value for Param2";
@@ -21,6 +29,10 @@ const msgIncorrectParam3 = "Incorrect value for Param3";
 const msgIncorrectParam4 = "Incorrect value for Param4";
 const msgIncorrectParam5 = "Incorrect value for Param5";
 const msgIncorrectParam6 = "Incorrect value for Param6";
+
+const msgBetweenZeroAndOne = "Param must be greater than 0 and less than 1.";
+const msgBetweenOneAndTwo = "Param must be greater than 1 and less than 2.";
+const msgNotZeroAddress = "Param cannot be zero address.";
 
 describe("Payload contract", function() {
     // Creating the instance and contract info for the payload
@@ -49,17 +61,17 @@ describe("Payload contract", function() {
         it("Test with no params", async function() {
             await payloadInstance.functions['M()']();
             assert.equal(
-                await payloadInstance.param1(),
+                (await payloadInstance.param1()).toString(),
                 param1,
                 msgIncorrectParam1
             );
             assert.equal(
-                await payloadInstance.param2(),
+                (await payloadInstance.param2()).toString(),
                 param2,
                 msgIncorrectParam2
             );
             assert.equal(
-                await payloadInstance.param3(),
+                (await payloadInstance.param3()).toString(),
                 param3,
                 msgIncorrectParam3
             );
@@ -69,12 +81,12 @@ describe("Payload contract", function() {
                 msgIncorrectParam4
             );
             assert.equal(
-                await payloadInstance.param5(),
+                (await payloadInstance.param5()).toString(),
                 param5,
                 msgIncorrectParam5
             );
             assert.equal(
-                await payloadInstance.param6(),
+                (await payloadInstance.param6()).toString(),
                 param6,
                 msgIncorrectParam6
             );
@@ -85,17 +97,17 @@ describe("Payload contract", function() {
         it("Test with 1 param", async function() {
             await payloadInstance.functions['M(uint256)'](updatedParam1);
             assert.equal(
-                await payloadInstance.param1(),
+                (await payloadInstance.param1()).toString(),
                 updatedParam1,
                 msgIncorrectParam1
             );
             assert.equal(
-                await payloadInstance.param2(),
+                (await payloadInstance.param2()).toString(),
                 param2,
                 msgIncorrectParam2
             );
             assert.equal(
-                await payloadInstance.param3(),
+                (await payloadInstance.param3()).toString(),
                 param3,
                 msgIncorrectParam3
             );
@@ -105,12 +117,12 @@ describe("Payload contract", function() {
                 msgIncorrectParam4
             );
             assert.equal(
-                await payloadInstance.param5(),
+                (await payloadInstance.param5()).toString(),
                 param5,
                 msgIncorrectParam5
             );
             assert.equal(
-                await payloadInstance.param6(),
+                (await payloadInstance.param6()).toString(),
                 param6,
                 msgIncorrectParam6
             );
@@ -121,17 +133,17 @@ describe("Payload contract", function() {
         it("Test with 2 params", async function() {
             await payloadInstance.functions['M(uint256,uint256)'](updatedParam1, updatedParam2);
             assert.equal(
-                await payloadInstance.param1(),
+                (await payloadInstance.param1()).toString(),
                 updatedParam1,
                 msgIncorrectParam1
             );
             assert.equal(
-                await payloadInstance.param2(),
+                (await payloadInstance.param2()).toString(),
                 updatedParam2,
                 msgIncorrectParam2
             );
             assert.equal(
-                await payloadInstance.param3(),
+                (await payloadInstance.param3()).toString(),
                 param3,
                 msgIncorrectParam3
             );
@@ -141,12 +153,12 @@ describe("Payload contract", function() {
                 msgIncorrectParam4
             );
             assert.equal(
-                await payloadInstance.param5(),
+                (await payloadInstance.param5()).toString(),
                 param5,
                 msgIncorrectParam5
             );
             assert.equal(
-                await payloadInstance.param6(),
+                (await payloadInstance.param6()).toString(),
                 param6,
                 msgIncorrectParam6
             );
@@ -157,17 +169,17 @@ describe("Payload contract", function() {
         it("Test with 3 params", async function() {
             await payloadInstance.functions['M(uint256,uint256,uint256)'](updatedParam1,updatedParam2,updatedParam3);
             assert.equal(
-                await payloadInstance.param1(),
+                (await payloadInstance.param1()).toString(),
                 updatedParam1,
                 msgIncorrectParam1
             );
             assert.equal(
-                await payloadInstance.param2(),
+                (await payloadInstance.param2()).toString(),
                 updatedParam2,
                 msgIncorrectParam2
             );
             assert.equal(
-                await payloadInstance.param3(),
+                (await payloadInstance.param3()).toString(),
                 updatedParam3,
                 msgIncorrectParam3
             );
@@ -177,12 +189,12 @@ describe("Payload contract", function() {
                 msgIncorrectParam4
             );
             assert.equal(
-                await payloadInstance.param5(),
+                (await payloadInstance.param5()).toString(),
                 param5,
                 msgIncorrectParam5
             );
             assert.equal(
-                await payloadInstance.param6(),
+                (await payloadInstance.param6()).toString(),
                 param6,
                 msgIncorrectParam6
             );
@@ -193,17 +205,17 @@ describe("Payload contract", function() {
         it("Test with 4 params", async function() {
             await payloadInstance.functions['M(uint256,uint256,uint256,address)'](updatedParam1, updatedParam2, updatedParam3, updatedParam4);
             assert.equal(
-                await payloadInstance.param1(),
+                (await payloadInstance.param1()).toString(),
                 updatedParam1,
                 msgIncorrectParam1
             );
             assert.equal(
-                await payloadInstance.param2(),
+                (await payloadInstance.param2()).toString(),
                 updatedParam2,
                 msgIncorrectParam2
             );
             assert.equal(
-                await payloadInstance.param3(),
+                (await payloadInstance.param3()).toString(),
                 updatedParam3,
                 msgIncorrectParam3
             );
@@ -213,12 +225,12 @@ describe("Payload contract", function() {
                 msgIncorrectParam4
             );
             assert.equal(
-                await payloadInstance.param5(),
+                (await payloadInstance.param5()).toString(),
                 param5,
                 msgIncorrectParam5
             );
             assert.equal(
-                await payloadInstance.param6(),
+                (await payloadInstance.param6()).toString(),
                 param6,
                 msgIncorrectParam6
             );
@@ -229,17 +241,17 @@ describe("Payload contract", function() {
         it("Test with 5 params", async function() {
             await payloadInstance.functions['M(uint256,uint256,uint256,address,uint256)'](updatedParam1, updatedParam2, updatedParam3, updatedParam4, updatedParam5);
             assert.equal(
-                await payloadInstance.param1(),
+                (await payloadInstance.param1()).toString(),
                 updatedParam1,
                 msgIncorrectParam1
             );
             assert.equal(
-                await payloadInstance.param2(),
+                (await payloadInstance.param2()).toString(),
                 updatedParam2,
                 msgIncorrectParam2
             );
             assert.equal(
-                await payloadInstance.param3(),
+                (await payloadInstance.param3()).toString(),
                 updatedParam3,
                 msgIncorrectParam3
             );
@@ -249,12 +261,12 @@ describe("Payload contract", function() {
                 msgIncorrectParam4
             );
             assert.equal(
-                await payloadInstance.param5(),
+                (await payloadInstance.param5()).toString(),
                 updatedParam5,
                 msgIncorrectParam5
             );
             assert.equal(
-                await payloadInstance.param6(),
+                (await payloadInstance.param6()).toString(),
                 param6,
                 msgIncorrectParam6
             );
@@ -265,17 +277,17 @@ describe("Payload contract", function() {
         it("Test with all params", async function() {
             await payloadInstance.functions['M(uint256,uint256,uint256,address,uint256,uint256)'](updatedParam1, updatedParam2, updatedParam3, updatedParam4, updatedParam5, updatedParam6);
             assert.equal(
-                await payloadInstance.param1(),
+                (await payloadInstance.param1()).toString(),
                 updatedParam1,
                 msgIncorrectParam1
             );
             assert.equal(
-                await payloadInstance.param2(),
+                (await payloadInstance.param2()).toString(),
                 updatedParam2,
                 msgIncorrectParam2
             );
             assert.equal(
-                await payloadInstance.param3(),
+                (await payloadInstance.param3()).toString(),
                 updatedParam3,
                 msgIncorrectParam3
             );
@@ -285,15 +297,108 @@ describe("Payload contract", function() {
                 msgIncorrectParam4
             );
             assert.equal(
-                await payloadInstance.param5(),
+                (await payloadInstance.param5()).toString(),
                 updatedParam5,
                 msgIncorrectParam5
             );
             assert.equal(
-                await payloadInstance.param6(),
+                (await payloadInstance.param6()).toString(),
                 updatedParam6,
                 msgIncorrectParam6
             );
+        })
+    });
+
+    describe("Invalid tests with method M", function() {
+        /**
+         * Invalid test using method M with param1
+         */
+        it("Invalid test using method M with param1", async function() {
+            await expect(
+                payloadInstance.functions['M(uint256,uint256,uint256,address,uint256,uint256)'](
+                    revertParam1, 
+                    updatedParam2, 
+                    updatedParam3, 
+                    updatedParam4, 
+                    updatedParam5, 
+                    updatedParam6
+                )
+            ).to.be.revertedWith(msgBetweenZeroAndOne);
+        })
+        /**
+         * Invalid test using method M with param2
+         */
+        it("Invalid test using method M with param2", async function() {
+            await expect(
+                payloadInstance.functions['M(uint256,uint256,uint256,address,uint256,uint256)'](
+                    updatedParam1, 
+                    revertParam2, 
+                    updatedParam3, 
+                    updatedParam4, 
+                    updatedParam5, 
+                    updatedParam6
+                )
+            ).to.be.revertedWith(msgBetweenZeroAndOne);
+        })
+        /**
+         * Invalid test using method M with param3
+         */
+        it("Invalid test using method M with param3", async function() {
+            await expect(
+                payloadInstance.functions['M(uint256,uint256,uint256,address,uint256,uint256)'](
+                    updatedParam1, 
+                    updatedParam2, 
+                    revertParam3, 
+                    updatedParam4, 
+                    updatedParam5, 
+                    updatedParam6
+                )
+            ).to.be.revertedWith(msgBetweenOneAndTwo);
+        })
+        /**
+         * Invalid test using method M with param4
+         */
+        it("Invalid test using method M with param4", async function() {
+            await expect(
+                payloadInstance.functions['M(uint256,uint256,uint256,address,uint256,uint256)'](
+                    updatedParam1, 
+                    updatedParam2, 
+                    updatedParam3, 
+                    revertParam4, 
+                    updatedParam5, 
+                    updatedParam6
+                )
+            ).to.be.revertedWith(msgNotZeroAddress);
+        })
+        /**
+         * Invalid test using method M with param5
+         */
+        it("Invalid test using method M with param5", async function() {
+            await expect(
+                payloadInstance.functions['M(uint256,uint256,uint256,address,uint256,uint256)'](
+                    updatedParam1, 
+                    updatedParam2, 
+                    updatedParam3, 
+                    updatedParam4, 
+                    revertParam5, 
+                    updatedParam6
+                )
+            ).to.be.revertedWith(msgBetweenZeroAndOne);
+        })
+        /**
+         * Invalid test using method M with param6
+         */
+        it("Invalid test using method M with param6", async function() {
+            await expect(
+                payloadInstance.functions['M(uint256,uint256,uint256,address,uint256,uint256)'](
+                    updatedParam1, 
+                    updatedParam2, 
+                    updatedParam3, 
+                    updatedParam4, 
+                    updatedParam5, 
+                    revertParam6
+                )
+            ).to.be.revertedWith(msgBetweenOneAndTwo);
         })
     });
 });
